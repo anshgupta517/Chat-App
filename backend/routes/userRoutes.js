50327e30086f767840afaad4e9f45d58b89ce64b
Ansh gupta <anshgupta51713@gmail.com>
2025-05-09 22:47:15 +0530

    jwt and routes updated

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,5 +4,6 @@ const { registerUser } = require('../controllers/userControllers');
 const router = express.Router();
 
 router.post('/register', registerUser);
+router.post('/login', loginUser);
 
 module.exports = router;

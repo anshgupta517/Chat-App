2fec829ec8988be491890f91ec9ae311481c8fee
Ansh gupta <anshgupta51713@gmail.com>
2025-05-09 23:37:03 +0530

    socket established

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
+const http = require('http');
+const socketIo = require('socket.io');
+
+const server = http.createServer(app);
+const io = socketIo(server);
+
 dotenv.config();
 
 const app = express();
@@ -19,6 +25,23 @@ mongoose.connect(process.env.MONGO_URI, {
 
 app.use('/api/message', require('./routes/messageRoutes'));
 app.use('/api/user', require('./routes/userRoutes'));
+
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+
+  socket.on('sendMessage', (message) => {
+    io.emit('message', message); 
+  });
+
+  socket.on('disconnect', () => {
+    console.log('Client disconnected');
+  });
+});
+
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 
 const PORT = process.env.PORT || 5000;
